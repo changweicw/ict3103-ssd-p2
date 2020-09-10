d271492dd3616b97b399f416f97ef9d6ac52d836
raphael chia <raphaelchia>
2020-09-10 16:04:11 +0800

    Template 2 done

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 from flask_wtf import CSRFProtect
 
-from appConfig import *
+from appConfig import DefaultConfig
 
 import wtform_validator as forms
 
 app = Flask(__name__,template_folder="templates")
+
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+
 # csrf = CSRFProtect(app)
 app.config.from_object(DefaultConfig)
-# mysql = MySQL(app)
+mysql = MySQL(app)
+
+
 
 
 @app.route('/', methods=['GET', 'POST'])
 def landing():
     title = "EhPlusMall"
-    return render_template('shared/index.html')
+    # return redirect("/shop/")
+    return render_template('landing.html')
+
+@app.route('/account',methods=['GET','POST'])
+def account():
+    return render_template('account/my-account.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,7 +37,7 @@ def login():
     #     # print(res)
     #     flash(login_form.password.data)
 
-    return render_template('logins/account_page.html')
+    return render_template('account/login.html')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -39,7 +48,9 @@ def registration():
         # return redirect(url_for('login'))
     return render_template('registration.html', form=registration_form)
 
-
+@app.route('/sell/dashboard')
+def sell_dashboard():
+    return render_template('sell/sell_dashboard.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
