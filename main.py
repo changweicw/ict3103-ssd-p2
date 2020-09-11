d127ad593e7a832ce4b47d49cdba41150b6e4edc
changweicw <changweicw88@gmail.com>
2020-09-11 10:44:56 +0800

    added jquery and swal alerts and regex validators

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from flask_login import LoginManager
+
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_mysqldb import MySQL
 from flask_wtf import CSRFProtect
@@ -9,7 +11,10 @@ import wtform_validator as forms
 app = Flask(__name__)
 # csrf = CSRFProtect(app)
 app.config.from_object(DefaultConfig)
+# login = LoginManager(app)
 mysql = MySQL(app)
+
+src = "http://127.0.0.1:8080/"
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,8 +26,8 @@ def login():
         #     mysql, login_form.username.data, login_form.password.data)
         # print(res)
         flash(login_form.password.data)
-
-    return render_template('login.html', form=login_form)
+        # return redirect(url_for('landing'))
+    return render_template('login.html', form=login_form, src=src)
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -30,8 +35,8 @@ def registration():
     registration_form = forms.RegistrationForm()
     if registration_form.validate_on_submit():
         flash(registration_form.password.data)
-        # return redirect(url_for('login'))
-    return render_template('registration.html', form=registration_form)
+        # return redirect(url_for('landing'))
+    return render_template('registration.html', form=registration_form, src=src)
 
 
 @app.route('/landing')
