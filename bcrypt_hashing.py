86933fc01c34d64a069a492169bcf189ebcd8ce6
changweicw <changweicw88@gmail.com>
2020-09-22 16:57:42 +0800

    storage and comparison of encrypted password

--- bcrypt_hashing.py
+++ bcrypt_hashing.py
@@ -3,9 +3,10 @@ import bcrypt as bcrypt
 
 def encrypt_password(entered_password):
 
-    encoded_enc_pw = entered_password.encode("utf-8")
+    encoded_enc_pw = entered_password.encode('utf-8')
     encrypted_pw = bcrypt.hashpw(encoded_enc_pw, bcrypt.gensalt())
-    return encrypted_pw.decode("utf-8")
+    print(encrypt_password)
+    return encrypted_pw.decode('utf-8')
 
 
 def password_validator(entered_password, hashed_password):
--- db_helper.py
+++ db_helper.py
@@ -1,7 +1,7 @@
 from models import User
 import logging
+from bcrypt_hashing import encrypt_password, password_validator
 from flask_mysqldb import MySQL
-
 
 logger = logging.getLogger(__name__)
 fh = logging.FileHandler('db.log')
@@ -9,46 +9,49 @@ fh.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 logger.addHandler(fh)
 logger.setLevel(logging.INFO)
 
-class db_helper:
-    
-    def __init__(self,mysql):
-        self.mysql=mysql
 
-    def check_login(self,email, password):
-        query = "SELECT * FROM user WHERE email = %s AND password = %s"
+class db_helper:
+
+    def __init__(self, mysql):
+        self.mysql = mysql
+
+    def check_login(self, email, password):
+        query = "SELECT * FROM users WHERE email = %s"
         cur = self.mysql.connection.cursor()
-        cur.execute(query, (email,password))
+        cur.execute(query, (email,))
         result = cur.fetchone()
-        if result:
-            logger.info(email+" just logged in")
+
+        if password_validator(password, result['password']):
+            logger.info(email + " just logged in")
             return result
         else:
-            logger.info(email+" failed to login")
+            logger.info(email + " failed to login")
             return None
-            
 
-    def email_exist(self,email):
-        query_checkemail="SELECT * from user where email = %s"
+    def email_exist(self, email):
+        query_checkemail = "SELECT * from user where email = %s"
         cur = self.mysql.connection.cursor()
-        cur.execute(query_checkemail,(email,))
+        cur.execute(query_checkemail, (email,))
         result = cur.fetchone()
         if result is not None:
             logger.info(email+" already exist")
             return True
         return False
 
-    def signup(self,user):
-        
+    def signup(self, user):
 
-        query_insert = "INSERT INTO user (fname,lname,email,password,password_salt,total_revenue,rating_avg,password_change_date,incorrect_login_count,user_join_date,removed) "
-        query_insert = query_insert+ "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        query_insert = "INSERT INTO users (fname,lname,email,password,password_salt,total_revenue,rating_avg,password_change_date,incorrect_login_count,user_join_date,removed) "
+        query_insert = query_insert + \
+            "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         cur = self.mysql.connection.cursor()
         try:
-            
-            cur.execute(query_insert,(user.fname,user.lname,user.email,user.password,user.salt,user.revenue,user.rating,user.passwordChangeDate,user.incorrectLoginCount,user.userJoinDate,user.removed))
-            self.mysql.connection.commit() 
+            enPass = encrypt_password(user.password)
+            print("Password Checker: " + enPass)
+            cur.execute(query_insert, (user.fname, user.lname, user.email, enPass, user.salt, user.revenue,
+                                       user.rating, user.passwordChangeDate, user.incorrectLoginCount, user.userJoinDate, user.removed))
+            self.mysql.connection.commit()
             logger.info("Register successful for "+user.fname)
-            
+
             return True
         except Exception as e:
             logger.warning("Register failed\n"+e)
